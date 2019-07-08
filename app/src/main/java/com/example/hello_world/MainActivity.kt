3224dd156c4503c789b6823b45938f81a8742154
RainZhong <1322030678@qq.com>
2019-07-08 15:57:45 +0800

    first commit

---
+++ app/src/main/java/com/example/hello_world/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.hello_world
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.CompoundButton
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var counter = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        seekBar.setProgress(50,true)
+        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar, p1: Int, p2: Boolean) {
+                seekBarText.setText("${p0.progress}")
+                imageView.alpha = p0.progress.toFloat() / 100
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+        })
+
+        mySwitch.setOnCheckedChangeListener(object: CompoundButton.OnCheckedChangeListener{
+            override fun onCheckedChanged(p0: CompoundButton?, p1: Boolean) {
+                if (p1)
+                {
+                    seekBar.setEnabled(false)
+                    changeButton.setEnabled(false)
+                }
+                else
+                {
+                    seekBar.setEnabled(true)
+                    changeButton.setEnabled(true)
+                }
+            }
+        })
+
+    }
+
+    fun changeButtonClick(v: View?)
+    {
+        when(v?.id)
+        {
+            R.id.changeButton->
+            {
+                if (counter == 0)
+                {
+                    helloWorldTextView.setText("Changed")
+                    counter += 1
+                }
+                else
+                {
+                    helloWorldTextView.setText("Hello World!")
+                    counter = 0
+                }
+            }
+        }
+
+    }
+
+
+}
